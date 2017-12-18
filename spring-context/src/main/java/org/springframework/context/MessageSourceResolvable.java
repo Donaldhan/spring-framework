f35ce9ee39934ea4270f6eef151a851b1542cd30
doanldhan <shaoqinghanah@qq.com>
2017-12-18 22:29:14 +0800

    add MessageSourceResolvable notes

--- spring-context/src/main/java/org/springframework/context/MessageSourceResolvable.java
+++ spring-context/src/main/java/org/springframework/context/MessageSourceResolvable.java
@@ -19,9 +19,9 @@ package org.springframework.context;
 /**
  * Interface for objects that are suitable for message resolution in a
  * {@link MessageSource}.
- *
+ *MessageSourceResolvable接口在MessageSource接口中用于消息解决。
  * <p>Spring's own validation error classes implement this interface.
- *
+ *Spring的字节错误验证validation类实现了此接口
  * @author Juergen Hoeller
  * @see MessageSource#getMessage(MessageSourceResolvable, java.util.Locale)
  * @see org.springframework.validation.ObjectError
@@ -32,12 +32,14 @@ public interface MessageSourceResolvable {
 	/**
 	 * Return the codes to be used to resolve this message, in the order that
 	 * they should get tried. The last code will therefore be the default one.
+	 * 返回消息关联的code
 	 * @return a String array of codes which are associated with this message
 	 */
 	String[] getCodes();
 
 	/**
 	 * Return the array of arguments to be used to resolve this message.
+	 * 返回消息中所有的参数
 	 * @return an array of objects to be used as parameters to replace
 	 * placeholders within the message text
 	 * @see java.text.MessageFormat
@@ -46,6 +48,7 @@ public interface MessageSourceResolvable {
 
 	/**
 	 * Return the default message to be used to resolve this message.
+	 * 返回默认的消息，如果没有则为null
 	 * @return the default message, or {@code null} if no default
 	 */
 	String getDefaultMessage();
