47d8eecfacb6c732e1ee6ecfcfd71d80da2b8d8c
doanldhan <shaoqinghanah@qq.com>
2017-12-19 09:32:47 +0800

    init ApplicationContext notes

--- spring-context/src/main/java/org/springframework/context/ApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/ApplicationContext.java
@@ -59,24 +59,28 @@ public interface ApplicationContext extends EnvironmentCapable, ListableBeanFact
 
 	/**
 	 * Return the unique id of this application context.
+	 * 返回应用上下文的id，没有则为null
 	 * @return the unique id of the context, or {@code null} if none
 	 */
 	String getId();
 
 	/**
 	 * Return a name for the deployed application that this context belongs to.
+	 * 返回应用上下文所属的部署应用名称，默认为空字符串
 	 * @return a name for the deployed application, or the empty String by default
 	 */
 	String getApplicationName();
 
 	/**
 	 * Return a friendly name for this context.
+	 * 返回上下文友好的展示name
 	 * @return a display name for this context (never {@code null})
 	 */
 	String getDisplayName();
 
 	/**
 	 * Return the timestamp when this context was first loaded.
+	 * 返回上下文第一次加载的时间戳
 	 * @return the timestamp (ms) when this context was first loaded
 	 */
 	long getStartupDate();
@@ -84,12 +88,14 @@ public interface ApplicationContext extends EnvironmentCapable, ListableBeanFact
 	/**
 	 * Return the parent context, or {@code null} if there is no parent
 	 * and this is the root of the context hierarchy.
+	 * 返回父上下文，如果没有父上下文，或这个是上下文的跟，则返回null。
 	 * @return the parent context, or {@code null} if there is no parent
 	 */
 	ApplicationContext getParent();
 
 	/**
 	 * Expose AutowireCapableBeanFactory functionality for this context.
+	 * 暴露上下文的AutowireCapableBeanFactory功能性
 	 * <p>This is not typically used by application code, except for the purpose of
 	 * initializing bean instances that live outside of the application context,
 	 * applying the Spring bean lifecycle (fully or partly) to them.
