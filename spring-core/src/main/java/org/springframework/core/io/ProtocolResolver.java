3fa1da785e95084cb9e7e3531f7ffb0a50e576a0
doanldhan <shaoqinghanah@qq.com>
2017-12-20 22:35:45 +0800

    add ProtocolResolver notes

--- spring-core/src/main/java/org/springframework/core/io/ProtocolResolver.java
+++ spring-core/src/main/java/org/springframework/core/io/ProtocolResolver.java
@@ -18,11 +18,12 @@ package org.springframework.core.io;
 
 /**
  * A resolution strategy for protocol-specific resource handles.
- *
+ *ProtocolResolver接口是一个特殊协议资源的解决策略处理器。
  * <p>Used as an SPI for {@link DefaultResourceLoader}, allowing for
  * custom protocols to be handled without subclassing the loader
  * implementation (or application context implementation).
- *
+ *默认资源加载器的系统包接口SPI，在没有子类资源加载器和应用上下文的实现的情况下，
+ *运行处理一般的资源协议。
  * @author Juergen Hoeller
  * @since 4.3
  * @see DefaultResourceLoader#addProtocolResolver
@@ -32,6 +33,7 @@ public interface ProtocolResolver {
 	/**
 	 * Resolve the given location against the given resource loader
 	 * if this implementation's protocol matches.
+	 * 如果协议的实现匹配，则根据资源加载器，解决给定位置的资源。
 	 * @param location the user-specified resource location
 	 * @param resourceLoader the associated resource loader
 	 * @return a corresponding {@code Resource} handle if the given location
