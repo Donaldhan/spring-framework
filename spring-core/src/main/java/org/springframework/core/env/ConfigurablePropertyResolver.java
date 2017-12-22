806a0151549c03e4a626f7d68fbef3a9e508296c
doanldhan <shaoqinghanah@qq.com>
2017-12-22 10:01:22 +0800

    add ConfigurablePropertyResolver comment

--- spring-core/src/main/java/org/springframework/core/env/ConfigurablePropertyResolver.java
+++ spring-core/src/main/java/org/springframework/core/env/ConfigurablePropertyResolver.java
@@ -23,7 +23,8 @@ import org.springframework.core.convert.support.ConfigurableConversionService;
  * types. Provides facilities for accessing and customizing the
  * {@link org.springframework.core.convert.ConversionService ConversionService}
  * used when converting property values from one type to another.
- *
+ *ConfigurablePropertyResolver是{@link PropertyResolver}的实现必须实现接口。当需要转化一个类型到另外一个类型时，
+ *为访问，和定制类型转换服务的便利。
  * @author Chris Beams
  * @since 3.1
  */
@@ -32,8 +33,10 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 	/**
 	 * Return the {@link ConfigurableConversionService} used when performing type
 	 * conversions on properties.
+	 * 当执行属性类型转换时，返回使用的可配转换服务ConfigurableConversionService。
 	 * <p>The configurable nature of the returned conversion service allows for
 	 * the convenient addition and removal of individual {@code Converter} instances:
+	 * 可配转换服务为添加和移除特殊的转换器实例，提供了便利。
 	 * <pre class="code">
 	 * ConfigurableConversionService cs = env.getConversionService();
 	 * cs.addConverter(new FooConverter());
@@ -46,10 +49,13 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 	/**
 	 * Set the {@link ConfigurableConversionService} to be used when performing type
 	 * conversions on properties.
+	 *设置可以执行类型转换的ConfigurableConversionService
 	 * <p><strong>Note:</strong> as an alternative to fully replacing the
 	 * {@code ConversionService}, consider adding or removing individual
 	 * {@code Converter} instances by drilling into {@link #getConversionService()}
 	 * and calling methods such as {@code #addConverter}.
+	 * 需要注意的是，如果我们想替换ConversionService，添加或移除类型转换器，可以从{@link #getConversionService()}，
+	 * 获取可配类型转换服务，比如{@code #addConverter}。
 	 * @see PropertyResolver#getProperty(String, Class)
 	 * @see #getConversionService()
 	 * @see org.springframework.core.convert.converter.ConverterRegistry#addConverter
@@ -58,11 +64,13 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 
 	/**
 	 * Set the prefix that placeholders replaced by this resolver must begin with.
+	 * 设置占位符前缀
 	 */
 	void setPlaceholderPrefix(String placeholderPrefix);
 
 	/**
 	 * Set the suffix that placeholders replaced by this resolver must end with.
+	 * 设置占位符后缀
 	 */
 	void setPlaceholderSuffix(String placeholderSuffix);
 
@@ -70,6 +78,7 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 	 * Specify the separating character between the placeholders replaced by this
 	 * resolver and their associated default value, or {@code null} if no such
 	 * special character should be processed as a value separator.
+	 * 设置被解决器替代占位符和其默认值之间的分割符，如果在处理的过程中，没有这个特殊分割符，则将是使用默认的或null最为分割符。
 	 */
 	void setValueSeparator(String valueSeparator);
 
@@ -79,15 +88,20 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 	 * resolution, i.e. that an exception will be thrown. A {@code true} value indicates
 	 * that unresolvable nested placeholders should be passed through in their unresolved
 	 * ${...} form.
+	 * 此方法用于设置，当遇见一个不能解决的嵌入在给定属性的占位符时，是否抛出异常。如果为false，则预示者，严格控制
+	 * 属性解决，比如抛出一个异常。true表示，如果没有解决${...}形式的属性的，则通过。
 	 * <p>Implementations of {@link #getProperty(String)} and its variants must inspect
 	 * the value set here to determine correct behavior when property values contain
 	 * unresolvable placeholders.
+	 * {@link #getProperty(String)}方法的实现，和他的变体，必须监视值集，已决定当属性值集包含未解决的占位符时，
+	 * 纠正占位符属性。
 	 * @since 3.2
 	 */
 	void setIgnoreUnresolvableNestedPlaceholders(boolean ignoreUnresolvableNestedPlaceholders);
 
 	/**
 	 * Specify which properties must be present, to be verified by
+	 * 设置需要校验的属性
 	 * {@link #validateRequiredProperties()}.
 	 */
 	void setRequiredProperties(String... requiredProperties);
@@ -96,6 +110,7 @@ public interface ConfigurablePropertyResolver extends PropertyResolver {
 	 * Validate that each of the properties specified by
 	 * {@link #setRequiredProperties} is present and resolves to a
 	 * non-{@code null} value.
+	 * 校验每个需要校验的属性，及{@link #setRequiredProperties}设置的属性，并解决非空值。
 	 * @throws MissingRequiredPropertiesException if any of the required
 	 * properties are not resolvable.
 	 */
