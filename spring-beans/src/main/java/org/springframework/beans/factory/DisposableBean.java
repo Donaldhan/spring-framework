fe5b681eda776972d99ce854772a59d66279e5d6
doanldhan <shaoqinghanah@qq.com>
2017-12-26 12:47:30 +0800

    add DisposableBean notes

--- spring-beans/src/main/java/org/springframework/beans/factory/DisposableBean.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/DisposableBean.java
@@ -21,10 +21,13 @@ package org.springframework.beans.factory;
  * on destruction. A BeanFactory is supposed to invoke the destroy
  * method if it disposes a cached singleton. An application context
  * is supposed to dispose all of its singletons on close.
- *
+ *DisposableBean接口的实现用于在析构时，释放资源。如果bean工厂销毁一个缓存单例bean，应该调用#destroy方法。
+ *应用上下文在关闭时，应该销毁所有的单例bean。
  * <p>An alternative to implementing DisposableBean is specifying a custom
  * destroy-method, for example in an XML bean definition.
  * For a list of all bean lifecycle methods, see the BeanFactory javadocs.
+ *DisposableBean的一种可选实现为，在基于XML的bean定义中，配置bean的destroy-method。更多关于所有的bean的
+ *生命周期方法，见BeanFactory的javadocs。
  *
  * @author Juergen Hoeller
  * @since 12.08.2003
@@ -35,9 +38,11 @@ public interface DisposableBean {
 
 	/**
 	 * Invoked by a BeanFactory on destruction of a singleton.
+	 * bean工厂在析构单例bean的时候调用此方法。
 	 * @throws Exception in case of shutdown errors.
 	 * Exceptions will get logged but not rethrown to allow
 	 * other beans to release their resources too.
+	 * 在关闭错误的情况下，异常将被log输出，而不是重新抛出以允许其他bean释放资源。
 	 */
 	void destroy() throws Exception;
 
